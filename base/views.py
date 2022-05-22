e6d131c7dd4afc7e18e34c40a07bd61c945f20ba
EduardPython <eduard.finexpert@gmail.com>
2022-05-22 13:48:17 +0200

    fix categories

--- base/views.py
+++ base/views.py
@@ -5,11 +5,6 @@ from django.urls import reverse_lazy
 from .models import Task
 
 
-# class TaskListView(ListView):
-#     model = Task
-#     template_name = "base/base.html"
-
-
 class TaskCreateView(CreateView):
     template_name = "base/task_create.html"
     model = Task
@@ -17,7 +12,7 @@ class TaskCreateView(CreateView):
     success_url = reverse_lazy("tasks")
 
     def get_context_data(self, *args, **kwargs):
-        context = super(TaskCreateView, self).get_context_data(**kwargs)
+        context = super(TaskCreateView, self).get_context_data(*args, **kwargs)
         context.update({
             'all_tasks': Task.objects.all().order_by("-created"),
         })
@@ -27,10 +22,16 @@ class TaskCreateView(CreateView):
 class CategoryListView(ListView):
     queryset = Task.objects.all()
     template_name = "categories.html"
-    extra_context = {
-        "family": Task.objects.filter(category=Task.CATEGORY_FAMILY).order_by("-dead_line"),
-        "work": Task.objects.filter(category=Task.CATEGORY_WORK).order_by("-dead_line"),
-        "garden": Task.objects.filter(category=Task.CATEGORY_GARDEN).order_by("-dead_line"),
-        "household": Task.objects.filter(category=Task.CATEGORY_HOUSEHOLD).order_by("-dead_line"),
-        "other": Task.objects.filter(category=Task.CATEGORY_OTHER).order_by("-dead_line")
-    }
+
+
+    def get_context_data(self, *args, **kwargs):
+        context=super(CategoryListView, self).get_context_data(*args, **kwargs)
+        extra_context = {
+            "family": Task.objects.filter(category=Task.CATEGORY_FAMILY).order_by("-dead_line"),
+            "work": Task.objects.filter(category=Task.CATEGORY_WORK).order_by("-dead_line"),
+            "garden": Task.objects.filter(category=Task.CATEGORY_GARDEN).order_by("-dead_line"),
+            "household": Task.objects.filter(category=Task.CATEGORY_HOUSEHOLD).order_by("-dead_line"),
+            "other": Task.objects.filter(category=Task.CATEGORY_OTHER).order_by("-dead_line")
+        }
+        context.update(extra_context)
+        return context
