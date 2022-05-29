83e114e548ef6e199ac97e75c56810ec83ff2f92
EduardPython <eduard.finexpert@gmail.com>
2022-05-29 11:38:45 +0200

    addet details to categories

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskCreateView, CategoryListView, TaskDone
+from .views import TaskCreateView, CategoryListView, TaskDone, TaskDetailView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
@@ -7,6 +7,8 @@ urlpatterns = [
     path("", TaskCreateView.as_view(), name="tasks"),
     path("categories/", CategoryListView.as_view(), name="categories"),
     path("task_done/<int:pk>", TaskDone.as_view(), name="task_done"),
+    path("task_detail/<int:pk>", TaskDetailView.as_view(), name="task_detail"),
+    # path("task_delete/<int:pk>/", DeleteTaskView.as_view(), name="delete_task"),
     # path("/g", TaskCreateView.as_view(), name="task_create"),
 ]
 
